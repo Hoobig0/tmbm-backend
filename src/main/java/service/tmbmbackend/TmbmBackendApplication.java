e2b9c9af45c065236a6b552f3c5facf18659270c
BOYOUNG-KANG <28287@naver.com>
2024-05-01 15:59:02 +0900

    init: spring initialize

---
+++ src/main/java/service/tmbmbackend/TmbmBackendApplication.java
@@ -0,0 +1,13 @@
+package service.tmbmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmbmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmbmBackendApplication.class, args);
+	}
+
+}
