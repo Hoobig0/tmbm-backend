b179a919ba73d5dc9a2a35356624f8cad142f13b
Cuzz <rygus9@naver.com>
2024-05-12 18:52:39 +0900

    feat: excel 데이터 로드 로직 작성

--- src/main/java/service/tmbmbackend/bootstrap/LocalDataSetup.java
+++ src/main/java/service/tmbmbackend/bootstrap/LocalDataSetup.java
@@ -1,10 +1,18 @@
 package service.tmbmbackend.bootstrap;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Component
 @Profile("local")
@@ -13,6 +21,27 @@ public class LocalDataSetup implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        System.out.println("안녕하세요!");
+        this.CCTVDataLoad();
+    }
+
+    private void CCTVDataLoad() {
+        try {
+            InputStream in = new ClassPathResource("cctv.xlsx").getInputStream();
+            Workbook workbook = new XSSFWorkbook(in);
+            Sheet sheet = workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                if(row.getRowNum() == 0){
+                    continue;
+                }
+                System.out.println(row.getCell(0).getStringCellValue()
+                        + row.getCell(1).getStringCellValue()
+                        + row.getCell(2).getStringCellValue()
+                        + row.getCell(11).getStringCellValue()
+                        + row.getCell(12).getStringCellValue());
+            }
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
