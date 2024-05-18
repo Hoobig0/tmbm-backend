a4f41bec4ff565b2d7c36317e5bcf6fc6bc0b921
Cuzz <rygus9@naver.com>
2024-05-18 17:21:06 +0900

    feat: CCTV 도메인에 생성자 추가

--- src/main/java/service/tmbmbackend/cctv/CCTV.java
+++ src/main/java/service/tmbmbackend/cctv/CCTV.java
@@ -3,23 +3,26 @@ package service.tmbmbackend.cctv;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 
 @Entity(name = "cctv")
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CCTV {
     @Id
     Long id;
     @Column
     Point point;
 
-    @Builder
-    public CCTV(Long id, Point point) {
+    public CCTV(Long id, double x, double y) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+        this.point = geometryFactory.createPoint(new Coordinate(x, y));
         this.id = id;
-        this.point = point;
     }
 }
