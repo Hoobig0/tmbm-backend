eb9f355001035bc4936250ad5db643b0f49e3b66
Cuzz <rygus9@naver.com>
2024-05-26 10:14:28 +0900

    feat: SQL 문 이용해서 데이터 로드하기.
    ApplicatoinRunner에서 CommandLineRunner로 바꿨는데 그 이유는 그냥 Exception를 throw하고 싶었기 때문입니다. ApplicationRunner가 인자를 사용하기 편하게 주긴 하지만 여기선 인자를 쓸 필요도 없어서 바꿔서 생기는 단점이 딱히 없다고 판단했습니다.

--- src/main/java/service/tmbmbackend/cctv/bootstrap/LocalDataSetup.java
+++ src/main/java/service/tmbmbackend/cctv/bootstrap/LocalDataSetup.java
@@ -1,52 +1,35 @@
 package service.tmbmbackend.cctv.bootstrap;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import service.tmbmbackend.cctv.CCTV;
-import service.tmbmbackend.cctv.repository.CCTVRepository;
-
-import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 @Profile("local")
 @RequiredArgsConstructor
-public class LocalDataSetup implements ApplicationRunner {
-    private final CCTVRepository cctvRepository;
+public class LocalDataSetup implements CommandLineRunner {
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public void run(ApplicationArguments args) {
-        this.CCTVDataLoad();
+    public void run(String... args) throws Exception {
+        executeInsertSql();
     }
 
-    private void CCTVDataLoad() {
-        try {
-            InputStream in = new ClassPathResource("cctv.xlsx").getInputStream();
-            Workbook workbook = new XSSFWorkbook(in);
-            Sheet sheet = workbook.getSheetAt(0);
-            for (Row row : sheet) {
-                if(row.getRowNum() == 0){
-                    continue;
-                }
-
-                Long id = Long.parseLong(row.getCell(0).getStringCellValue());
-                double x = Double.parseDouble(row.getCell(11).getStringCellValue());
-                double y = Double.parseDouble(row.getCell(12).getStringCellValue());
-
-                CCTV cctv = new CCTV(id, x, y);
-                cctvRepository.save(cctv);
-            }
-            workbook.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void executeInsertSql() throws Exception {
+        Path path = Paths.get(new ClassPathResource("insert-statement.sql").getURI());
+        try (Stream<String> stream = Files.lines(path)) {
+            String sql = stream.collect(Collectors.joining(System.lineSeparator()));
+            jdbcTemplate.execute(sql);
+        } catch (Exception e) {
+            throw new Exception("Failed to load and execute SQL file", e);
         }
     }
 }
